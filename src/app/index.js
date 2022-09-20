a8eec3d231c1b6db4fb3027dab89102f3d4b0dc0
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-09-20 08:32:21 +0300

    feat drow a table

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,22 @@
-export const App = () => {
-    return <h1>Hello</h1>
-}
+import { Table } from 'antd';
+import React, { useState } from 'react';
+import { getUsersData } from '../api/api';
+import 'antd/dist/antd.css';
+import columns from '../table/columns';
+import newUsersData from '../table/row';
+
+const App = () => {
+  const [userData, setUsersData] = useState([]);
+  React.useEffect(() => {
+    getUsersData()
+      .then((res) => {
+        const data = newUsersData(res);
+        setUsersData(data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  return <Table columns={columns} dataSource={userData} bordered />;
+};
+
+export default App;
