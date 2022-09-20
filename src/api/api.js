58d9a416cfdf100780ccce4f83c75e8aabde4bdf
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-09-20 08:36:21 +0300

    fix: fix api

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,13 @@
-export const Api = () => {
+export const getUsersData = () => {
 	return fetch('http://localhost:8080/api/users', {
 		method: 'GET',
-		mode: 'no-cors', 
 		headers: { 'Content-Type': 'application/json'},
 })
 .then(res => {
-if(res.ok) {
+	if(res.ok) {
 		return res.json();
 	} else {
 		return Promise.reject(res.status);
 	}
 })
-.then((res) => console.log(res))
 }
